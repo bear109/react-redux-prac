ca8208e81053deb3d3051f6e1d61a2a70186045e
bear109 <jokerbear109@gmail.com>
2022-09-08 16:43:54 +0900

    git repo name change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import CounterContainer from './containers/CounterContainer';
 import TodosContainer from './containers/TodosContainer';
 
+//git repo name change
 function App() {
   return (
     <div className="App">
